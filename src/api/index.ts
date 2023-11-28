62317cdb814abeede01cd4c7733e6365952bdca3
Sean O'Connor <so@seano.dev>
2023-11-27 17:05:20 -0700

    Build UMCU API wrapper

---
+++ src/api/index.ts
@@ -0,0 +1,29 @@
+import { ProductionType } from './models';
+
+export async function get<T>(path: string = ''): Promise<T> {
+  const url = `https://api.umcu.app/productions${path}`;
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok)
+      throw new Error(`Error: ${response.status} - ${response.statusText}`);
+
+    const body: T = await response.json();
+    return body;
+  } catch (error: any) {
+    throw new Error(`Error fetching data: ${error.message}`);
+  }
+}
+
+export async function getAllProductions() {
+  return get<ProductionType[]>();
+}
+
+export async function getProduction(slug: string) {
+  return get<ProductionType>(`/${slug}`);
+}
+
+export async function getNextProduction(date: string | undefined = undefined) {
+  if (!date) return get<ProductionType>(`/next`);
+  return get<ProductionType>(`/next?date=${date}`);
+}
---
+++ src/api/models.ts
@@ -0,0 +1,8 @@
+export type ProductionType = {
+  tmdbId: number;
+  title: string;
+  overview: string | undefined;
+  releaseDate: string | undefined;
+  poster: string | undefined;
+  slug: string;
+};
