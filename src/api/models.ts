1f89da999228f5ce6e090838069cb04582dd0046
Sean O'Connor <so@seano.dev>
2023-12-03 12:08:05 -0700

    Update ProductionType model

--- src/api/models.ts
+++ src/api/models.ts
@@ -1,8 +1,9 @@
 export type ProductionType = {
-  tmdbId: number;
+  tmdb_id: number;
   title: string;
   overview: string | undefined;
-  releaseDate: string | undefined;
+  release_date: string | undefined;
   poster: string | undefined;
+  media_type: 'MOVIE' | 'TV';
   slug: string;
 };
