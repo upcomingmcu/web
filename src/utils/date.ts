7f21e745b370e209de98ec2393d089177c77f441
Sean O'Connor <so@seano.dev>
2023-12-03 16:39:13 -0700

    Create date util functions

---
+++ src/utils/date.ts
@@ -0,0 +1,11 @@
+import { parseISO } from 'date-fns';
+import { formatInTimeZone } from 'date-fns-tz';
+
+export const parseDateString = (dateString: string) => parseISO(dateString);
+
+export const formatDateString = (dateString: string) =>
+  formatInTimeZone(
+    parseDateString(dateString),
+    'America/New_York',
+    'MMMM do, yyyy'
+  );
